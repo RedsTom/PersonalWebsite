9232f817b25e27a0a18e3cb8a0b905b1ed1519c4
RedsTom <tom.frefre.13@gmail.com>
2023-09-15 23:31:34 +0200

    Update Contact.vue

--- src/components/sections/Contact.vue
+++ src/components/sections/Contact.vue
@@ -8,7 +8,7 @@ const body = ref('');
 const send = () => {
   if (subject.value && body.value) {
     // @ts-ignore
-    window.location = `mailto:contact@redstom.me?subject=${subject.value}&body=${body.value.replace('\n', '%0D%0A')}`;
+    window.location = `mailto:tom.frefre.13@gmail.com?subject=${subject.value}&body=${body.value.replace('\n', '%0D%0A')}`;
   }
 }
 </script>
